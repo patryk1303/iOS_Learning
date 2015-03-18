620f2b1a93a644c26465f7b2d5bbb64eeba511aa
Mac05 <mac01@iMac-Mac05.local>
2015-03-18 09:59:42 +0100

    18.03.2015 - varsion for this day

--- Starter Project/ViewController.m
+++ Starter Project/ViewController.m
@@ -10,6 +10,10 @@
 #import "City.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *textLabel;
+@property (assign,nonatomic) NSInteger menuButtonTapCount;
+- (IBAction)menuButtonDownTapped:(id)sender;
+- (IBAction)menuButtonUpTapped:(id)sender;
 
 @end
 
@@ -18,15 +22,20 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    self.menuButtonTapCount = 0;
+    
     City *city1 = [City cityWithName:@"Koszalin" andCitizensCount:@(109170)];
     City *city2 = [City cityWithName:@"Szczecin" andCitizensCount:@(410000)];
     City *city3 = [City cityWithName:@"Łódź" andCitizensCount:@(750000)];
     City *city4 = [City cityWithName:@"Warszawa" andCitizensCount:@(1800000)];
     NSArray *cityList = [[NSArray alloc] initWithObjects:city1,city2,city3,city4, nil];
-    
+
     for (City *city in cityList) {
         [city showYourCity];
     }
+    
+    self.textLabel.text = @"Hell low, NOTHING!";
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,4 +44,16 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)menuButtonUpTapped:(id)sender {
+    self.menuButtonTapCount++;
+    [self updateLabelMenuButtonTappedCount];
+}
+
+- (void)updateLabelMenuButtonTappedCount {
+    self.textLabel.text = [NSString stringWithFormat:@"Przycisk naciśnięty\n%@ razy", @(self.menuButtonTapCount)];
+}
+- (IBAction)menuButtonDownTapped:(id)sender {
+    self.menuButtonTapCount--;
+    [self updateLabelMenuButtonTappedCount];
+}
 @end
